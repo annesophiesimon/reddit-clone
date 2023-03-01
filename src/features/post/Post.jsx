5ffa35ed7427613e3fcc47677cb2779967103de7
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2023-03-01 14:44:35 -0500

    group Post features in right folder and components

---
+++ src/features/post/Post.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable react/prop-types */
+import Vote from '../../components/Vote';
+import { convertTime, checkURL } from '../../helpers';
+import { fetchPosts } from './postSlice';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useEffect } from 'react';
+
+const Post = () => {
+  const { posts, isLoading } = useAppSelector((state) => state.post);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts('r/sewing')); // remove hard coded
+  }, []);
+
+  if (isLoading) {
+    return <div>Data loading</div>; // Add spinner
+  }
+  return (
+    <>
+      {posts.length > 0 ? (
+        posts
+          .filter((post) => checkURL(post.url))
+          .map((post) => (
+            <div key={post.id} className=" container mx-auto  bg-white">
+              <div className="flex py-5 shadow-lg border border-grey-50 rounded-xl max-w-[700px] mb-10">
+                <div className="flex flex-col">
+                  <div className="flex items-start">
+                    <section>
+                      <article>
+                        <div className="px-5 mb-5">
+                          <h1 className="font-bold text-[#212427] text-sm">{post.author}</h1>
+                          <p className="text-slate-500 text-xs">{convertTime(post.created_utc)}</p>
+                          <h1 className="my-4  text-[#212427]"> {post.title}</h1>
+                        </div>
+                        <div className="flex items-center justify-center mb-8">
+                          <img
+                            className="justify-items-center"
+                            alt={post.title}
+                            src={post.url}></img>
+                        </div>
+                        <hr className="mx-20 mb-10" />
+                      </article>
+                    </section>
+                  </div>
+                  <div className="flex justify-between text-sm text-slate-500 px-5">
+                    <Vote ups={post.ups} />
+                    <button>
+                      <p value={post.id}>
+                        {post.num_comments} {post.num_comments > 1 ? `comments` : `comment`}
+                      </p>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+      ) : (
+        <p> There is no post yet </p>
+      )}
+    </>
+  );
+};
+
+export default Post;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import Header from '../components/Header';
-import Post from '../components/Post';
+import Post from '../features/post/Post';
 import Categories from '../components/Categories';
 import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 // eslint-disable-next-line no-unused-vars
 import { fetchPosts, fetchPostsByTerm, fetchComments } from '../features/post/postSlice';
 import { fetchSubreddit } from '../features/subreddit/subredditSlice';
-import { checkURL } from '../helpers/index';
 
 const Home = () => {
-  const { posts, isLoading, isError, message } = useAppSelector((state) => state.post);
   const { subreddits, isCategoriesLoading } = useAppSelector((state) => state.subreddit);
   const dispatch = useAppDispatch();
 
@@ -22,6 +20,7 @@ const Home = () => {
 
   const handleClick = (e) => {
     setSubreddit(e.target.value);
+    console.log(subreddit);
   };
 
   const handleSubmit = (e) => {
@@ -29,24 +28,15 @@ const Home = () => {
     dispatch(fetchPostsByTerm(term));
   };
 
-  const onToggleComments = (e) => {
-    console.log('test', e.target);
-    //dispatch(fetchComments(permalink));
-  };
-
   // const handleOnClick = (permalink) => {
   //   dispatch(fetchComments(permalink));
   //   setIsHidden(!isHidden);
   // };
   useEffect(() => {
-    // if (isError) {
-    //   console.log(message);
-    // }
-    dispatch(fetchPosts(subreddit));
     dispatch(fetchSubreddit());
-  }, [isError, message, subreddit]);
+  }, []);
 
-  if (isLoading && isCategoriesLoading) {
+  if (isCategoriesLoading) {
     return <div>Data loading</div>; // Add spinner
   }
   return (
@@ -55,13 +45,7 @@ const Home = () => {
       <div className="container mx-auto max-w-[1100px]">
         <main className="mt-8 flex flex-col md:flex-row justify-between m-4 ">
           <div className="flex flex-col max-w-[900px] md:mr-20">
-            {posts.length > 0 ? (
-              posts
-                .filter((post) => checkURL(post.url))
-                .map((post) => <Post key={post.id} post={post} toggleComments={onToggleComments} />)
-            ) : (
-              <p> There is no post yet </p>
-            )}
+            <Post />
           </div>
           <Categories
             categories={subreddits}
