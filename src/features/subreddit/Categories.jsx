670c35bcf583ceb8d4758a16db2521eacb3e8815
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2023-03-01 16:44:07 -0500

    group Subreddit  features in right the folder and component

---
+++ src/features/subreddit/Categories.jsx
@@ -0,0 +1,34 @@
+import { fetchSubreddit } from './subredditSlice';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useEffect } from 'react';
+
+const Categories = (subreddit) => {
+  const { handleClick } = subreddit;
+  const { subreddits, isCategoriesLoading } = useAppSelector((state) => state.subreddit);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchSubreddit());
+  }, []);
+
+  if (isCategoriesLoading) {
+    return <div>Data loading</div>; // Add spinner
+  }
+  return (
+    <aside className="shadow-lg border border-grey-50 rounded-xl min-w-[300px] h-full">
+      <h1 className="font-bold p-5">Categories</h1>
+      <ul className="my-2">
+        {subreddits &&
+          subreddits.map((subreddit, index) => (
+            <li key={index} className="px-5 py-2 hover:bg-slate-500/[.06]">
+              <button onClick={handleClick} value={subreddit.display_name_prefixed}>
+                {subreddit.display_name}
+              </button>
+            </li>
+          ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import Header from '../components/Header';
 import Post from '../features/post/Post';
-import Categories from '../components/Categories';
+import Categories from '../features/subreddit/Categories';
 import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../app/hooks';
-// eslint-disable-next-line no-unused-vars
-import { fetchPosts, fetchPostsByTerm, fetchComments } from '../features/post/postSlice';
+import { useAppDispatch } from '../app/hooks';
+import { fetchPostsByTerm } from '../features/post/postSlice';
 import { fetchSubreddit } from '../features/subreddit/subredditSlice';
 
 const Home = () => {
-  const { subreddits, isCategoriesLoading } = useAppSelector((state) => state.subreddit);
+  //const { subreddits, isCategoriesLoading } = useAppSelector((state) => state.subreddit);
   const dispatch = useAppDispatch();
 
   const [term, setTerm] = useState('');
@@ -20,7 +19,6 @@ const Home = () => {
 
   const handleClick = (e) => {
     setSubreddit(e.target.value);
-    console.log(subreddit);
   };
 
   const handleSubmit = (e) => {
@@ -28,30 +26,19 @@ const Home = () => {
     dispatch(fetchPostsByTerm(term));
   };
 
-  // const handleOnClick = (permalink) => {
-  //   dispatch(fetchComments(permalink));
-  //   setIsHidden(!isHidden);
-  // };
   useEffect(() => {
     dispatch(fetchSubreddit());
   }, []);
 
-  if (isCategoriesLoading) {
-    return <div>Data loading</div>; // Add spinner
-  }
   return (
     <>
       <Header handleChange={handleChange} handleSubmit={handleSubmit} term={term} />
       <div className="container mx-auto max-w-[1100px]">
         <main className="mt-8 flex flex-col md:flex-row justify-between m-4 ">
           <div className="flex flex-col max-w-[900px] md:mr-20">
-            <Post />
+            <Post subreddit={subreddit} />
           </div>
-          <Categories
-            categories={subreddits}
-            isCategoriesLoading={isCategoriesLoading}
-            handleClick={handleClick}
-          />
+          <Categories handleClick={handleClick} />
         </main>
       </div>
     </>
